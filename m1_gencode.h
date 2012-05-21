c132f504cbfd59191716cdb911a0f1ddd610fe2c
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-21 19:35:33 +0200

    refactoring

--- m1_gencode.h
+++ m1_gencode.h
@@ -3,13 +3,15 @@
 
 #include "m1_ast.h"
 #include "m1_symtab.h"
+#include "m1_compiler.h"
+
 /* to pass registers; I42 -> no=42, type='I' */
 typedef struct m1_reg {
     unsigned char no;
     char          type;
 } m1_reg;
 
-extern void gencode(m1_chunk *ast);
+extern void gencode(M1_compiler *comp, m1_chunk *ast);
 
 #endif
 
