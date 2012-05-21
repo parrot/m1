e2f8465173e57b48f073ceb9b0933850bdeb85bc
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-21 19:34:28 +0200

    refactoring ast

--- m1_ast.c
+++ m1_ast.c
@@ -6,8 +6,11 @@ AST node constructors
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 #include "m1_ast.h"
 #include "m1_symtab.h"
+
+#include "m1_ann.h"
 
 static void *
 m1_malloc(size_t size) {
@@ -19,14 +22,10 @@ m1_malloc(size_t size) {
     return mem;   
 }
 
-m1_assignment *
-assign(char *lhs, char *rhs) {
-    m1_assignment *a = (m1_assignment *)m1_malloc(sizeof(m1_assignment));
-    return a;
-}
 
+/*@modiefies nothing @*/ 
 m1_chunk *
-chunk(int rettype, char *name, m1_expression *block) {
+chunk(int rettype, NOTNULL(char *name), m1_expression *block) {
     m1_chunk *c = (m1_chunk *)m1_malloc(sizeof(m1_chunk));
     c->rettype  = rettype;
     c->name     = name;
@@ -45,35 +44,81 @@ expression(m1_expr_type type) {
 
 void 
 expr_set_num(m1_expression *e, double v) {
+    assert(e->type == EXPR_NUMBER); 
     e->expr.floatval = v;
-    e->type = EXPR_NUMBER; 
-    e->sym  = sym_enter_num(&floats, v);  
+    e->sym           = sym_enter_num(&floats, v);  
 }
 
 void 
 expr_set_int(m1_expression *e, int v) {
+    assert(e->type == EXPR_INT);
     e->expr.intval = v;
-    e->type = EXPR_INT;
-    e->sym = sym_enter_int(&ints, v);
+    e->sym         = sym_enter_int(&ints, v);
 }
 
-static m1_binexpr *
-binexpr(m1_binop op) {
-    m1_binexpr *e = (m1_binexpr *)m1_malloc(sizeof(m1_binexpr));
-    e->op         = op;
-    return e;    
+m1_expression *
+number(double value) {
+	m1_expression *expr = expression(EXPR_NUMBER);
+	expr_set_num(expr, value);
+	return expr;	
 }
 
-void 
-expr_set_binexpr(m1_expression *node, m1_expression *e1, 
-                 m1_binop op, m1_expression *e2) 
-{
-    
-    
-    node->expr.b = binexpr(op);
-    node->expr.b->left  = e1;
-    node->expr.b->right = e2;    
-    node->type = EXPR_BINARY;   
+m1_expression *
+integer(int value) {
+	m1_expression *expr = expression(EXPR_INT);
+	expr_set_int(expr, value);
+	return expr;	
+}
+m1_expression *
+string(char *str) {
+	m1_expression *expr = expression(EXPR_STRING);
+	expr_set_string(expr, str);
+	return expr;	
+}
+
+m1_object *
+arrayindex(m1_expression *index) {
+	m1_object *obj = object(OBJECT_INDEX);
+	obj_set_index(obj, index);
+	return obj;	
+}
+
+m1_object *
+objectfield(char *field) {
+	m1_object *obj = object(OBJECT_FIELD);
+	obj_set_ident(obj, field);
+	return obj;	
+}
+
+m1_object *
+objectderef(char *field) {
+	m1_object *obj = object(OBJECT_DEREF);
+	obj_set_ident(obj, field);
+	return obj;	
+}
+               
+m1_expression *
+unaryexpr(m1_unop op, m1_expression *e) {
+	m1_expression *expr = expression(EXPR_UNARY);
+	expr_set_unexpr(expr, e, op);
+	return expr;
+}	
+                
+m1_expression *
+binexpr(m1_expression *e1, m1_binop op, m1_expression *e2) {
+	m1_expression *expr = expression(EXPR_BINARY);
+	expr->expr.b = (m1_binexpr *)m1_malloc(sizeof(m1_binexpr));
+    expr->expr.b->op    = op;
+    expr->expr.b->left  = e1;
+    expr->expr.b->right = e2;       
+    return expr;    
+}
+
+m1_expression *
+printexpr(m1_expression *e) {
+	m1_expression *expr = expression(EXPR_PRINT);
+	expr_set_expr(expr, e);
+	return expr;	
 }
 
 static m1_unexpr *
@@ -86,23 +131,44 @@ unexpr(m1_expression *node, m1_unop op) {
 
 void
 expr_set_unexpr(m1_expression *node, m1_expression *exp, m1_unop op) {
+    assert(node->type == EXPR_UNARY);   
     node->expr.u = unexpr(exp, op);
-    node->type   = EXPR_UNARY;   
 }
+
 void 
 expr_set_funcall(m1_expression *node, m1_funcall *f) {
+    assert(node->type == EXPR_FUNCALL);   
     node->expr.f = f;
-    node->type = EXPR_FUNCALL;   
 }
 
-m1_funcall *
+m1_expression *
 funcall(char *name) {
-    m1_funcall *f = (m1_funcall *)m1_malloc(sizeof(m1_funcall));
-    f->name       = name;
-    return f;   
+	m1_expression *expr = expression(EXPR_FUNCALL);
+	expr->expr.f       = (m1_funcall *)m1_malloc(sizeof(m1_funcall));
+	expr->expr.f->name = name;
+    return expr;   
 }
 
-void 
+
+static m1_const *
+const_decl(data_type type, char *name, m1_expression *expr) {
+    m1_const *c = (m1_const *)m1_malloc(sizeof(m1_const));
+    c->type     = type;
+    c->name     = name;
+    c->value    = expr;
+    return c;    
+}
+
+
+
+m1_expression *
+constdecl(data_type type, char *name, m1_expression *e) {
+	m1_expression *expr = expression(EXPR_CONSTDECL);
+	expr->expr.c = const_decl(type, name, e);
+	return expr;	
+}
+
+static void 
 expr_set_for(m1_expression *node, m1_expression *init,
              m1_expression *cond, m1_expression *step,
              m1_expression *stat) 
@@ -115,19 +181,55 @@ expr_set_for(m1_expression *node, m1_expression *init,
     node->expr.o->block = stat;
 }   
 
-void 
+m1_expression *
+forexpr(m1_expression *init, m1_expression *cond, m1_expression *step, m1_expression *stat) {
+	m1_expression *expr = expression(EXPR_FOR);
+	expr_set_for(expr, init, cond, step, stat);	
+	return expr;
+}
+
+
+
+m1_expression *
+inc_or_dec(m1_expression *obj, m1_unop optype) {	
+	m1_expression *expr = expression(EXPR_UNARY);
+	expr_set_unexpr(expr, obj, optype);
+	return expr;
+}
+
+m1_expression *
+assignexpr(m1_expression *lhs, m1_expression *rhs) {
+	m1_expression *expr = expression(EXPR_ASSIGN); 
+	expr_set_assign(expr, lhs, rhs);
+    return expr;
+}
+
+m1_expression *
+objectexpr(m1_object *obj, m1_expr_type type) {
+	m1_expression *expr = expression(type);
+	expr_set_obj(expr, obj); 
+	return expr;
+}
+
+m1_expression *
+returnexpr(m1_expression *retexp) {
+	m1_expression *expr = expression(EXPR_RETURN);
+	expr_set_expr(expr, retexp);
+	return expr;	
+}
+
+static void 
 expr_set_while(m1_expression *node, m1_expression *cond, m1_expression *block) {
-    node->expr.w = (m1_whileexpr *)m1_malloc(sizeof(m1_whileexpr));
-    
+    node->expr.w        = (m1_whileexpr *)m1_malloc(sizeof(m1_whileexpr));    
     node->expr.w->cond  = cond;
     node->expr.w->block = block;                            
 }   
 
-void
+static void
 expr_set_if(m1_expression *node, m1_expression *cond, 
-            m1_expression *ifblock, m1_expression *elseblock) {
-    node->expr.i = (m1_ifexpr *)m1_malloc(sizeof(m1_ifexpr));            
-    
+            m1_expression *ifblock, m1_expression *elseblock) 
+{
+    node->expr.i = (m1_ifexpr *)m1_malloc(sizeof(m1_ifexpr));              
     node->expr.i->cond      = cond;
     node->expr.i->ifblock   = ifblock;
     node->expr.i->elseblock = elseblock;
@@ -145,7 +247,7 @@ expr_set_obj(m1_expression *node, m1_object *obj) {
 
 void 
 expr_set_assign(m1_expression *node, m1_expression *lhs, m1_expression *rhs) {
-    node->expr.a = (m1_assignment *)m1_malloc(sizeof(m1_assignment));
+    node->expr.a      = (m1_assignment *)m1_malloc(sizeof(m1_assignment));
     node->expr.a->lhs = lhs;
     node->expr.a->rhs = rhs;        
 }
@@ -153,7 +255,7 @@ expr_set_assign(m1_expression *node, m1_expression *lhs, m1_expression *rhs) {
 void 
 expr_set_string(m1_expression *node, char *str) {
     node->expr.str = str;
-    node->sym = sym_enter_str(&strings, str, 0);
+    node->sym      = sym_enter_str(&strings, str, 0);
 }
 
 void 
@@ -176,8 +278,8 @@ object(m1_object_type type) {
 m1_structfield *
 structfield(char *name, data_type type) {
     m1_structfield *fld = (m1_structfield *)m1_malloc(sizeof(m1_structfield));
-    fld->name = name;
-    fld->type = type;
+    fld->name           = name;
+    fld->type           = type;
     return fld;   
 }
 
@@ -189,26 +291,18 @@ newstruct(char *name, m1_structfield *fields) {
     return str;   
 }
 
-m1_const *
-const_decl(data_type type, char *name, m1_expression *expr) {
-    m1_const *c = (m1_const *)m1_malloc(sizeof(m1_const));
-    c->type = type;
-    c->name = name;
-    c->value = expr;
-    return c;    
-}
 
-void
-expr_set_const_decl(m1_expression *node, data_type type, 
-                    char *name, m1_expression *expr) 
-{
-    node->expr.c = const_decl(type, name, expr);       
-}
-
-void
+static void
 expr_set_var_decl(m1_expression *node, data_type type, m1_var *decl) {
     node->expr.v = decl;
     decl->type   = type;    
+}
+
+m1_expression *
+vardecl(data_type type, m1_var *v) {
+	m1_expression *expr = expression(EXPR_VARDECL);
+	expr_set_var_decl(expr, type, v);
+	return expr;	
 }
 
 m1_var *
@@ -216,4 +310,25 @@ var(char *name) {
     m1_var *v = (m1_var *)m1_malloc(sizeof(m1_var));
     v->name   = name;
     return v;    
+}
+
+m1_expression *
+ifexpr(m1_expression *cond, m1_expression *ifblock, m1_expression *elseblock) {
+	m1_expression *expr = expression(EXPR_IF);
+	expr_set_if(expr, cond, ifblock, elseblock);
+	return expr;
+}
+
+m1_expression *
+whileexpr(m1_expression *cond, m1_expression *block) {
+	m1_expression *expr = expression(EXPR_WHILE);
+	expr_set_while(expr, cond, block);
+	return expr;	
+}
+
+m1_expression *
+dowhileexpr(m1_expression *cond, m1_expression *block) {
+	m1_expression *expr = expression(EXPR_DOWHILE);
+	expr_set_while(expr, cond, block);
+	return expr;	
 }
