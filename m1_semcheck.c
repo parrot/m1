1553f51b4c3ca569af17f81d181688fa8d957177
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-22 19:44:36 +0100

    add some code to check.c

--- m1_semcheck.c
+++ m1_semcheck.c
@@ -1,7 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "m1_semcheck.h"
 
 
-void 
-check(struct M1_compiler *comp, struct m1_chunk *ast) {
+static void
+check_expr(M1_compiler *comp, m1_expression *chunk) {
 	
+}
+
+
+static data_type
+check_chunk(M1_compiler *comp, m1_chunk *chunk) {
+	
+	m1_expression *iter = chunk->block;
+	
+	while (iter != NULL) {
+		check_expr(comp, iter);
+		iter = iter->next;	
+	}
+	return chunk->rettype;
+}
+
+void 
+check(M1_compiler *comp, m1_chunk *ast) {
+	m1_chunk *iter = ast;
+	
+	while (iter != NULL) {
+		check_chunk(comp, iter);
+		iter = iter->next;	
+	}	
 }
