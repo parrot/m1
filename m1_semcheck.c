cb2ede4343ba23d023b3da380c94c559600c5a91
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-22 23:22:59 +0100

    minor additions to sem check

--- m1_semcheck.c
+++ m1_semcheck.c
@@ -1,23 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "m1_semcheck.h"
+#include "m1_ast.h"
 
+static data_type
+check_object(M1_compiler *comp, m1_object *obj) {
+	switch (obj->type) {
+		case OBJECT_MAIN:
+		
+		case OBJECT_FIELD:
+		case OBJECT_INDEX:
+		case OBJECT_DEREF:
+			
+/*		case OBJECT_SCOPE:*/
+		default:
+			exit(EXIT_FAILURE);
+	}	
+}
 
-static void
-check_expr(M1_compiler *comp, m1_expression *chunk) {
-	
+static data_type
+check_expr(M1_compiler *comp, m1_expression *expr) {
+	data_type t;
+	switch (expr->type) {
+		case EXPR_NUMBER:
+			return TYPE_NUM;
+		case EXPR_INT:
+			return TYPE_INT;
+		case EXPR_BINARY: 
+		{
+			data_type ltype = check_expr(comp, expr->expr.b->left);
+			data_type rtype = check_expr(comp, expr->expr.b->right);
+			
+			if (ltype != rtype) {
+				fprintf(stderr, "Incompatible types in binary expression\n");
+				++comp->errors;
+			}			
+			return ltype;
+		}
+		case EXPR_UNARY:
+    	case EXPR_FUNCALL:
+    	case EXPR_ASSIGN:
+    	case EXPR_IF:
+    	case EXPR_WHILE:
+    	case EXPR_DOWHILE:
+    	case EXPR_FOR:
+    	case EXPR_RETURN:
+    	case EXPR_NULL:
+    	case EXPR_DEREF:
+    	case EXPR_ADDRESS:
+		case EXPR_OBJECT:
+			return check_object(comp, expr->expr.t);
+    	case EXPR_BREAK:
+    		break;
+	    case EXPR_STRING:
+	    	return TYPE_STRING;
+	    	
+		case EXPR_CONSTDECL:
+   		case EXPR_VARDECL:
+		case EXPR_M0BLOCK:
+    	case EXPR_PRINT:
+    		break;
+		default:
+			break;	
+	}
+	return t;
 }
 
 
 static data_type
 check_chunk(M1_compiler *comp, m1_chunk *chunk) {
-	
+	data_type type;
 	m1_expression *iter = chunk->block;
 	
 	while (iter != NULL) {
-		check_expr(comp, iter);
+		type = check_expr(comp, iter);
+		if (iter->type == EXPR_RETURN) {
+			if (chunk->rettype != type) {
+				fprintf(stderr, "Return type of function '%s' does not match type of returned expression\n", chunk->name);
+				++comp->errors;	
+			}
+		}
 		iter = iter->next;	
 	}
+
 	return chunk->rettype;
 }
 
@@ -26,7 +91,7 @@ check(M1_compiler *comp, m1_chunk *ast) {
 	m1_chunk *iter = ast;
 	
 	while (iter != NULL) {
-		check_chunk(comp, iter);
+		(void)check_chunk(comp, iter);
 		iter = iter->next;	
 	}	
 }
