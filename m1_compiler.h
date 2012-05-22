52a68f4e8ca11c760b8259af96b4ce994e7c18dc
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-22 09:42:04 +0100

    update compiler

--- m1_compiler.h
+++ m1_compiler.h
@@ -1,14 +1,17 @@
 #ifndef __M1_COMPILER__
 #define __M1_COMPILER__
 
-#include "m1_ast.h"
+
 
 typedef struct M1_compiler {
-	unsigned         errors;
-	struct m1_chunk *ast;	
-	int				 constindex;
-	int              label;
-	int				 regs[4];
+	unsigned         		errors;
+	struct m1_chunk 	   *ast;	
+	int				 		constindex;
+	int              		label;
+	int				 		regs[4];
+	struct m1_symboltable  *floats;
+	struct m1_symboltable  *ints;
+	struct m1_symboltable  *strings;
 	
 } M1_compiler;
 
