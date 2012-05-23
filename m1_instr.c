a2fe60301a3c6ca7d2a773271f13da94113f9b92
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-23 17:06:13 +0100

    start with write_xxx functions for instructions. Easy to finish; easy to get started hacking on M1! Contributors welcome

--- m1_instr.c
+++ m1_instr.c
@@ -49,6 +49,89 @@ char const * const m0_instr_names[] = {
     
 };
 
+#define OUT stdout
+
+static void
+write_noop(m0_instr *i) {
+    fprintf(OUT, "noop");    
+}
+
+static void
+write_goto(m0_instr *i) {
+    fprintf(OUT, "goto");   
+}
+
+static void
+write_goto_if(m0_instr *i) {
+    fprintf(OUT, "goto_if");   
+}
+
+static void
+write_goto_chunk(m0_instr *i) {
+    fprintf(OUT, "goto_chunk");   
+}
+
+/* XX other write functions go here. */
+
+
+void
+write_instructions(m0_instr *i) {
+    while (i != NULL) {
+        fprintf(OUT, "\t");
+        switch (i->opcode) {
+            case M0_NOOP: write_noop(i); break;
+            case M0_GOTO: write_goto(i); break;
+            case M0_GOTO_IF: write_goto_if(i); break;
+            case M0_GOTO_CHUNK: write_goto_chunk(i); break;
+            
+            /* XXX todo: implement all functions to write. Also, fix operands at some point. 
+    M0_ADD_I,
+    M0_ADD_N,
+    M0_SUB_I,
+    M0_SUB_N,
+    M0_MULT_I,
+    M0_MULT_N,
+    M0_DIV_I,
+    M0_DIV_N,
+    M0_MOD_I,
+    M0_MOD_N,
+    M0_ITON,
+    M0_NTOI,
+    M0_ASHR,
+    M0_LSHR,
+    M0_SHL,
+    M0_AND,
+    M0_OR,
+    M0_XOR,
+    M0_GC_ALLOC,
+    M0_SYS_ALLOC,
+    M0_SYS_FREE,
+    M0_COPY_MEM,
+    M0_SET,
+    M0_SET_IMM,
+    M0_DEREF,
+    M0_SET_REF,
+    M0_SET_BYTE,
+    M0_GET_BYTE,
+    M0_SET_WORD,
+    M0_GET_WORD,
+    M0_CSYM,
+    M0_CCALL_ARG,
+    M0_CCALL_RET,
+    M0_CCALL,
+    M0_PRINT_S,
+    M0_PRINT_I,
+    M0_PRINT_N,
+    M0_EXIT
+    */
+            default:
+                fprintf(OUT, "unknown op!");
+                exit(EXIT_FAILURE);
+        }
+        fprintf(OUT, "\n");
+        i = i->next;   
+    }   
+}
 
 m0_instr *
 instr(char op, char arg1, char arg2, char arg3) {
@@ -58,9 +141,12 @@ instr(char op, char arg1, char arg2, char arg3) {
         fprintf(stderr, "cant alloc mem for instr");
         exit(EXIT_FAILURE);
     }
-    i->opcode = op;
+    
+    i->opcode      = op;
     i->operands[0] = arg1;
     i->operands[1] = arg2;
     i->operands[2] = arg3;
+    i->next        = NULL;
+    
     return i;    
 }
