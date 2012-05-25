bc4fa886042406352bdd038ea9ad7ea72d572b08
Klaas-Jan Stol <parrotcode@gmail.com>
2012-05-25 10:36:40 +0100

    add stack to compiler and remove a magic number

--- m1_compiler.h
+++ m1_compiler.h
@@ -1,19 +1,21 @@
 #ifndef __M1_COMPILER__
 #define __M1_COMPILER__
 
-
+#define NUM_TYPES   4
 
 typedef struct M1_compiler {
 	unsigned         		errors;
 	struct m1_chunk 	   *ast;	 /* root of the AST */
 	int				 		constindex; /* constant table index counter */
 	int              		label; /* label generator */
-	unsigned    	 		regs[4]; /* for the register allocator */
+	unsigned    	 		regs[NUM_TYPES]; /* for the register allocator */
 	
 	struct m1_symboltable  *floats;
 	struct m1_symboltable  *ints;
 	struct m1_symboltable  *strings;
 	struct m1_symboltable  *globals;
+	
+	struct m1_stack        *breakstack; /* for handling break statements */
 	
 } M1_compiler;
 
