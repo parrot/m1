369aa5e96a720b66d8a4325d831420ea69849874
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2012-06-09 11:08:26 +0800

    switch back to m0, also delete unused t/caller.m1, all test pass here

--- run_m1.sh
+++ run_m1.sh
@@ -12,6 +12,6 @@ echo "running $1..."
 ./m1 $1 2>/dev/null > $filename.m0
 [ -s $filename.m0 ] || { echo "nok..outputs a empty file $filename.m0 when compiling $1"; exit 1; }
 ./m0_assembler.pl $filename.m0 >/dev/null
-./m0_interp.pl $filename.m0b || { echo "nok..$1 exits unexpectedly"; exit 1; }
+./m0 $filename.m0b || { echo "nok..$1 exits unexpectedly"; exit 1; }
 echo "ok..$1 exits normally"
 exit 0
