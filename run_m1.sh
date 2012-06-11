46c19c6fced183e4e1772a1468d938531928e31d
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2012-06-11 21:34:42 +0800

    improve run_m1.sh

--- run_m1.sh
+++ run_m1.sh
@@ -12,5 +12,5 @@ echo "running $1..."
 ./m1 $1 2>/dev/null > $filename.m0
 [ -s $filename.m0 ] || { echo "nok..outputs a empty file $filename.m0 when compiling $1"; exit 1; }
 ./m0_assembler.pl $filename.m0 >/dev/null
-./m0 $filename.m0b
+./m0 $filename.m0b || { exit 1; }
 exit 0
